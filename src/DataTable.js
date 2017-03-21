5a9658f605380da8094a712c463fb62abad7cf22
Jesse Buchanan <hello@jessebuchanan.ca>
2017-03-21 13:36:47 -0400

    rename App to DataTable, render it

---
+++ src/DataTable.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './DataTable.css';
+
+class DataTable extends Component {
+  render() {
+    return (
+      <div className="abcd">
+        abcd
+      </div>
+    );
+  }
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import DataTable from './DataTable';
 import './index.css';
 
 ReactDOM.render(
-  <App />,
+  <div>
+    <h1>Sales</h1>
+    <DataTable data={data} />
+  </div>,
   document.getElementById('root')
 );
